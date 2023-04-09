eda390bc89e9e0ff99e172057ea22592538a1972
Fabian Fröschl <fabianf96@googlemail.com>
2023-04-09 22:59:42 +0200

    frontend: created, styled and populated table, delete and refresh buttons work

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,70 @@
 const port = 3000
 
-async function fetchAllAlbums() {
-  let albums = await fetch(`http://localhost:${port}/api/albums`)
-  console.log(await albums.json())
+function start() {
+  updateAlbumTable()
+  enableRefreshBtn()
+  enableAddBtn()
 }
 
-// document.getElementById("album-data").innerHTML = 
-fetchAllAlbums()
+async function fetchAllAlbums() {
+  let albums = await (await fetch(`http://localhost:${port}/api/albums`)).json()
+  console.log(albums)
+  return albums
+}
+
+async function deleteAlbum(id) {
+  console.log(`http://localhost:${port}/api/albums/${id}`)
+  let deleted = await fetch(`http://localhost:${port}/api/albums/${id}`, {
+    method: "DELETE"
+  })
+  return deleted
+}
+
+async function updateAlbumTable() {
+  let albums = await fetchAllAlbums()
+  let tbl = document.getElementById("album-data")
+  if (albums.length === 0) return tbl.innerHTML = "<tr><td>No data</td></tr>"
+  tbl.innerHTML = ''
+  albums.forEach(album => {
+    tbl.innerHTML +=
+      `
+      <tr>
+        <td class="title">${album.title}</td>
+        <td class="artist">${album.artist}</td>
+        <td>${album.year}</td>
+        <td><button class="update-btn" album-id=${album._id}>Edit</button></td>
+        <td><button class="delete-btn" album-id=${album._id}>Delete</button></td>
+      </tr>
+      `})
+  enableDeleteBtns()
+  enableUpdateBtns()
+}
+
+function enableUpdateBtns() {
+  let btns = document.querySelectorAll(".update-btn")
+  btns.forEach(btn => {
+
+  })
+}
+
+function enableDeleteBtns() {
+  let btns = document.querySelectorAll(".delete-btn")
+  btns.forEach(btn => {
+    btn.addEventListener("click", async (event) => {
+      let deleted = await deleteAlbum(btn.getAttribute("album-id"))
+      await updateAlbumTable()
+    })
+  })
+}
+
+function enableAddBtn() {
+
+}
+
+function enableRefreshBtn() {
+  document.querySelector("#refresh").addEventListener("click", async (event) => {
+    await updateAlbumTable()
+  })
+}
+
+start()
